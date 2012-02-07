7403b70612ff0ac9a1af8f796b03e296e943f218
Jacob Brandt <brandtjt23@uww.edu>
2012-02-07 13:10:23 -0600

    Added rspec tags

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,12 +8,12 @@ describe PagesController do
   end
 
   describe "GET 'home'" do
-    it "should be successful" do
+    it "should be successful", :chapter3 => "true" do
       get 'home'
       response.should be_success
     end
     
-    it "should have the right title" do
+    it "should have the right title", :chapter3 => "true" do
       get 'home'
       response.should have_selector("title",
                                     :content => @base_title + " | Home")
@@ -21,12 +21,12 @@ describe PagesController do
   end
 
   describe "GET 'contact'" do
-    it "should be successful" do
+    it "should be successful", :chapter3 => "true" do
       get 'contact'
       response.should be_success
     end
 
-    it "should have the right title" do
+    it "should have the right title", :chapter3 => "true" do
       get 'contact'
       response.should have_selector("title",
                                     :content => @base_title + " | Contact")
@@ -34,12 +34,12 @@ describe PagesController do
   end
 
   describe "GET 'about'" do
-    it "should be successful" do
+    it "should be successful", :chapter3 => "true" do
       get 'about'
       response.should be_success
     end
 
-    it "should have the right title" do
+    it "should have the right title", :chapter3 => "true" do
       get 'about'
       response.should have_selector("title",
                                     :content => @base_title + " | About")
