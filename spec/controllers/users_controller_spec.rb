0c80f4095f094978c285c39b0cbb7700b6b81f73
Jacob Brandt <brandtjt23@uww.edu>
2012-02-28 13:48:24 -0600

    Finished first cut of the User model

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,12 +4,12 @@ describe UsersController do
   render_views
 
   describe "GET 'new'" do
-    it "should be successful" do
+    it "should be successful", :chapter5 => "true" do
       get 'new'
       response.should be_success
     end
 
-    it "should have the right title" do
+    it "should have the right title", :chapter5 => "true" do
       get 'new'
       response.should have_selector("title", :content => "Sign up")
     end
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,28 +2,42 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
 
-  it "should have a Home page at '/'" do
+  it "should have a Home page at '/'", :chapter5 => "true" do
     get '/'
     response.should have_selector('title', :content => "Home")
   end
 
-  it "should have a Contact page at '/contact'" do
+  it "should have a Contact page at '/contact'", :chapter5 => "true" do
     get '/contact'
     response.should have_selector('title', :content => "Contact")
   end
 
-  it "should have an About page at '/about'" do
+  it "should have an About page at '/about'", :chapter5 => "true" do
     get '/about'
     response.should have_selector('title', :content => "About")
   end
 
-  it "should have a Help page at '/help'" do
+  it "should have a Help page at '/help'", :chapter5 => "true" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end
   
-  it "should have a signup page at '/signup'" do
+  it "should have a signup page at '/signup'", :chapter5 => "true" do
       get '/signup'
       response.should have_selector('title', :content => "Sign up")
   end
+  
+  it "should have the right links on the layout", :chapter5 => "true" do
+      visit root_path
+      click_link "About"
+      response.should have_selector('title', :content => "About")
+      click_link "Help"
+      response.should have_selector('title', :content => "Help")
+      click_link "Contact"
+      response.should have_selector('title', :content => "Contact")
+      click_link "Home"
+      response.should have_selector('title', :content => "Home")
+      click_link "Sign up now!"
+      response.should have_selector('title', :content => "Sign up")
+    end
 end
